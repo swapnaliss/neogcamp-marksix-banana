f7f4e1736ca4cc5cf74760516da304cd9675c20d
swapnaliss <swapnalisbhargude123@gmail.com>
2022-09-17 16:24:32 +0530

    feat :function added for click event

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ var inputText = document.querySelector("#input-text");
 var outputText = document.querySelector("#output-text");
 var translateButton = document.querySelector("#translate-btn");
 
+function clickHandler(){
+    console.log('clickHandler');
+                              
+}
+
+
+translateButton.addEventListener("click",clickHandler)
