20293b9fe05a431f067eda10e94ce5f9e3868890
swapnaliss <swapnalisbhargude123@gmail.com>
2022-10-04 21:16:01 +0530

    feat: validation apply

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var inputText = document.querySelector("#input-text");
 var outputText = document.querySelector("#output-text");
 var translateButton = document.querySelector("#translate-btn");
-
+var errorMessage =document.querySelector("#errorMessage");
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function getTranslationURL(userInput){
@@ -16,14 +16,18 @@ function insertOutputToTextArea(translatedText){
 function clickHandler(){
 
  var userInput = inputText.value;
+ if(userInput === ""){
+     errorMessage.innerText="Field cannot be empty!";
+ }else{
   fetch(getTranslationURL(userInput))
     .then((response) => response.json())
     .then(data =>{
         var translatedText = data.contents.translated;
         insertOutputToTextArea(translatedText)
+        errorMessage.innerText = "";
     })
     .catch();
-
+ }
 
 }
 
